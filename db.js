de26c5dd7e64ee138cf5830652a75394378b7306
Nozpal-Nam675 <nozpalnam@gmail.com>
2025-07-17 22:42:52 +1000

    Fix merge conflict in db.js

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('forms.db');
 
@@ -38,44 +37,4 @@ module.exports = {
     });
   }
 };
-=======
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('forms.db');
 
-// Create table if it doesn't exist
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS customer_forms (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT,
-      email TEXT,
-      message TEXT,
-      date_submitted TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
-  `);
-});
-
-module.exports = {
-  insertForm: (name, email, message) => {
-    return new Promise((resolve, reject) => {
-      db.run(
-        `INSERT INTO customer_forms (name, email, message) VALUES (?, ?, ?)`,
-        [name, email, message],
-        function (err) {
-          if (err) reject(err);
-          else resolve();
-        }
-      );
-    });
-  },
-
-  getAllForms: () => {
-    return new Promise((resolve, reject) => {
-      db.all(`SELECT * FROM customer_forms ORDER BY date_submitted DESC`, [], (err, rows) => {
-        if (err) reject(err);
-        else resolve(rows);
-      });
-    });
-  }
-};
->>>>>>> 1c0bcd8e1bb6c197f8f3a229ccb08936abc54a51
